3748531ede06c0e04e2b79e0929caa5b92edd004
Oskar Legner <oskarlegner@gmail.com>
2022-12-29 19:38:32 +0100

    Add update function

--- car.py
+++ car.py
@@ -17,3 +17,26 @@ class Car:
         # Set the car's acceleration and deceleration
         self.acceleration = 0.1
         self.deceleration = 0.1
+
+
+    def update(self):
+        # Handle keyboard input
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.speed += self.acceleration
+        elif keys[pygame.K_DOWN]:
+            self.speed -= self.deceleration
+        else:
+            self.speed *= self.deceleration
+
+        # Update the car's position
+        self.rect.x += self.speed
+
+        # Handle collision with the edges of the screen
+        if self.rect.left < 0 or self.rect.right > 640:
+            self.speed = 0
+            self.rect.center = (320, 240)
+
+    def draw(self):
+        # Draw the car image
+        self.screen.blit(self.image, self.rect)
