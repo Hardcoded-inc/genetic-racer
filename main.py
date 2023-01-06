8b9ea2ecdebf1a27bc4fdd26da06659be3ad1a48
sandis-jakub-m <j.macioszek@sandis.io>
2023-01-07 00:41:54 +0100

    Add menu

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import sys
 import pygame
 from game import Game
 
+def print_menu():
+    print("1. standardowa rozgrywka")
+    print("2. tryb ai")
+    print("3. Option 3")
+    print("4. Option 4")
+    print("0. exit")
 
 if __name__ == "__main__":
 
@@ -9,14 +15,24 @@ if __name__ == "__main__":
     ai_mode = "-ai" in args
     debug = "-d" in args
 
-    if(ai_mode):
-        print("Started in AI Mode")
-        game = Game(ai_mode=True, debug=debug)
-        game.run()
-
-    else:
-        game = Game(debug=debug)
-        game.run()
+    while True:
+        print_menu()
+        choice = input("Enter a number (1-5): ")
+        if choice == "1":
+            game = Game(debug=debug)
+            game.run()
+        elif choice == "2":
+            print("Started in AI Mode")
+            game = Game(ai_mode=True, debug=debug)
+            game.run()
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "0":
+            exit(0)
+        else:
+            print("Invalid choice. Try again.")
 
 
 
